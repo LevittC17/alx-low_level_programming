898edf9b40d9e24b1c25c7bf1614ebcfe2ecea1e
LevittC17 <LeewednesdayC17@protonmail.com>
2022-09-08 19:59:30 -0700

    Task 6 - Printing the size of data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * main - printing size of data types
+ * Pointers - for referencing the data type location
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	printf("Size of char, %d\n", sizeof(char*));
+	printf("Size of int, %d\n", sizeof(int*));
+	printf("Size of float, %d", sizeof(float*));
+	printf("Size of long int, %d\n", sizeof(long int*));
+	printf("size of double, %d\n", sizeof(double*);
+	return (0);
