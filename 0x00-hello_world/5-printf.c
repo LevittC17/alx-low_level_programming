0a68b94fab4c7d01ef68a0c2e4ed6af58a05645a
LevittC17 <LeewednesdayC17@protonmail.com>
2022-09-08 19:30:18 -0700

    Task 5 - Using Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+ * main - using printf to output a message
+ * Return: 0 if exits properly
+*/
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return 0;
+}
