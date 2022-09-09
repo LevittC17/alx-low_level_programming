c5ba1ead71840761ab7b0154bac41d9fa0585865
LevittC17 <LeewednesdayC17@protonmail.com>
2022-09-08 19:15:43 -0700

    Task 4 - Outputing with the puts() function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Using puts() to print a message
- *
- * return 0 - the program should end with a zero
-*/
-int main()
+ * main() - Using puts() to output a message
+ * Return 0, as file should end with 0
+ */
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle")
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
