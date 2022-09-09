c2490d6c9b100cb6b40adca7cd0f0a14da2fa4db
LevittC17 <LeewednesdayC17@protonmail.com>
2022-09-09 03:21:29 -0700

    Advanced Task 2 - GCC -Wal Option with no warning

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point to code
+ * Return: 1 if it exits correctly
+ */
+int main()
+{
+	printf("and that piece of art is useful\" Dora Korpar, 2015-10-19");
+	return 1;
+}
