cc73dfd92dfb0976157ed9d4b0d4b23cec790645
LevittC17 <LeewednesdayC17@protonmail.com>
2022-09-08 19:08:23 -0700

    Task 4 - C Program, outputing with puts()

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Using puts() to print a message
+ *
+ * return 0 - the program should end with a zero
+*/
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle")
+	return 0;
+}
