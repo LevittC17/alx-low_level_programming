426698e8c0c4529f18d56f268c16a1b0389d405d
LevittC17 <LeewednesdayC17@protonmail.com>
2022-09-08 19:20:29 -0700

    Task 4 - Outputing with the puts() function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - printing the message "Programming is like building a multilingual puzzle
- * 
+ * main - printing a message
  * Return: 0 if exited properly
- * */
+ */
  
 int main(void)
 {
