e02af153f63b85b4215e6787f1c4054e4a993f39
LevittC17 <LeewednesdayC17@protonmail.com>
2022-09-12 12:33:38 -0700

    Advanced Task 101 - Generate Assembly code of C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - prints message with printf
- * Return: 1 if exited properly
- */
+ * main - Entry point
+ * Return: (1) if exits properly
+*/
 int main(void)
 {
-	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	return (1);
+		write(2,
+				"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+			return (1);
 }
