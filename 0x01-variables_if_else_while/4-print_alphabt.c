c095a6f2c6f4afb00fbaacb079fabdf325171234
LevittC17 <LeewednesdayC17@protonmail.com>
2022-09-12 09:22:19 -0700

    Task 4 - Print but some alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 always of exits properly
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a == 'q' || a == 'e')
+			continue;
+	}
+	putchar('\n');
+	return (0);
+}
