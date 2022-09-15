e7fd61548f3c2bb5fe2ed7c24a422fbbfc1c51bc
LevittC17 <LeewednesdayC17@protonmail.com>
2022-09-15 14:15:10 -0700

    Task 1 - Printing alphabets using a function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function to print alphabets
+ * 
+ * Return: 0 always if exits properly
+ */
+void print_alphabet(void)
+{
+	char c;
+	
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+_putchar('\n');
+return (0);
+}
