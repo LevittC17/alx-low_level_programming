46ccc66e1e7b5680037405c32f275db6253c2621
LevittC17 <LeewednesdayC17@protonmail.com>
2022-09-15 13:17:21 -0700

    Task 0 - Functions / Printing _putchar with a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - Entry point to my code
+ * main - Entry Point
  *
- * Return: 0 always if exits correctly
+ * Return: 0 always if exits properly
  */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_puthcar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 	return (0);
 }
