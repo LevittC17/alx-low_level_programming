daadd10ad811666d73247f9dc9229e1f65337dba
LevittC17 <LeewednesdayC17@protonmail.com>
2022-09-16 07:43:25 -0700

    Printing multiplication table of 9 from 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_times_table - Printing the 9 times table beginning with zero
+ * times_table - Printing the 9 times table beginning with zero
  */
-void print_times_table(void)
+void times_table(void)
 {
 	int num, mult, prod;
 
