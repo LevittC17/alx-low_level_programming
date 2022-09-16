b4395f9d4debcbadf13b82d9844e08c9a69f2852
LevittC17 <LeewednesdayC17@protonmail.com>
2022-09-16 05:54:09 -0700

    Task 4 - Checking fot alphabetic character

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,7 +5,7 @@
  * @c: int type to be used for the argument of this function
  * Return: 0 always if exits properly
  */
-int _isalphabet(int c)
+int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
