7df841dab05f1eb23e172458d21faa91ba85e541
LevittC17 <LeewednesdayC17@protonmail.com>
2022-09-16 05:52:23 -0700

    Task 4 - Checking fot alphabetic character

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _isalpha - Function to see if c is a letter, lowercase or uppercase
+ * _isalphabet - Function to see if c is a letter, lowercase or uppercase
  * @c: int type to be used for the argument of this function
  * Return: 0 always if exits properly
  */
-int _isalpha(int c)
+int _isalphabet(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
