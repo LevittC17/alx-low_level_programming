1e06ba3bf673ceea92d59b853d830fbe23e3c6b7
LevittC17 <LeewednesdayC17@protonmail.com>
2022-09-19 12:10:30 -0700

    Return the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _strlen - functin to return length of a string
+* @s: The parameter
+*
+* Return: returns length
+*/
+int _strlen(char *s)
+{
+	int count;
+	
+	while (*p != '\0\)
+	{
+		count++;
+		p++;
+	}
+	return count;
+}
