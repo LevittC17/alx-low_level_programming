d2c32351359265344bbd9c685468a567ec3ba118
LevittC17 <LeewednesdayC17@protonmail.com>
2022-09-30 07:25:09 -0700

    Added a README.md file, a Main.h and git

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* printf - print output of argc
+*
+* Return: On success 0
+*/
+int main(int argc, char *argv[])
+{
+	printf("argc = %d\n", argc);
+
+	return (0);
+}
