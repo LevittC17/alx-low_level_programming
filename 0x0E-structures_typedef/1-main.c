931d7219aebb7a32abf5d525a4cf638ff3a65e53
LevittC17 <LeewednesdayC17@protonmail.com>
2022-10-12 18:29:14 +0000

    Initialize a variable of type struct

---
+++ 0x0E-structures_typedef/1-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+* main - check code
+*
+* Return: On success 0
+*/
+int main(void)
+{
+	struct dog my_dog;
+
+	int_dog(&my_dog, "HackerSploit", 3.5, "Jay");
+	printf("My name is %s, amd I am %.1f :) - woof!\n my_dog.name, my_dog.name);
+	return (0);
+}
