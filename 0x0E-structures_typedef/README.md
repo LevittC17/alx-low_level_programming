a7c5902785580ee2dcd05618fc3bc090127aa5dd
LevittC17 <LeewednesdayC17@protonmail.com>
2022-10-12 18:11:04 +0000

    Characteristics of a dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,3 @@
+# STRUCTURES, UNIONS, ENUM, TYPEDEF
+
+* TASK 0: Defining a new Structure with some elements
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,26 @@
+#ifndef "DOG_H"
+#define "DOG_H"
+
+/**
+* struct dog - characteristics of a dog
+* @name: first element
+* @age: second element
+* @owner: third element
+*
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+
+
+
+
+
+
+#endif /* DOG_H */
