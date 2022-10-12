2a2713ebef96a20ef9203b93e570394e201beb8f
LevittC17 <LeewednesdayC17@protonmail.com>
2022-10-12 18:40:28 +0000

    git readmefile dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,14 +1,20 @@
-#ifndef "DOG_H"
-#define "DOG_H"
+#ifndef _DOG_H_
+#define _DOG_H_
 
 /**
-* struct dog - characteristics of a dog
-* @name: first element
-* @age: second element
-* @owner: third element
-*
-*/
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
 
+/**
+ * struct dog - struct that stores some information of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: struct called "dog" that stores its name, its age
+ * and the name of its owner.
+ */
 struct dog
 {
 	char *name;
@@ -16,20 +22,9 @@ struct dog
 	char *owner;
 };
 
-/**
-* dog_t - typedefinition for struct dog
-*/
-
-typedef struct dog dog_t;
-
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
 
-
-
-#endif /* DOG_H */
+#endif
