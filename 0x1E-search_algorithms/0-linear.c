4e693cd425a325e31bcc71d1a56c99b1d145eb2f
LevittC17 <LeewednesdayC17@protonmail.com>
2023-07-12 15:55:41 +0000

    Linear search algorithm

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * Linear search algorithm to search for
+ * a value in an array of integers
+ * Argument: array, size, value
+ *
+ * @array: input array
+ * @size_t: size of array
+ * @value: item to search in
+ * Return: EXIT_SUCCESS
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	for (size_t i = 0; i < size; i++)
+	{
+		printf("Checking array[%zu] = %d\n", i, array[i])
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+	return (-i);
+}
